ae8b484a65c051307ae6e96027fe00d934f60cb9
Miguel Angel Sánchez Contreras <87663011+S4nchezM4@users.noreply.github.com>
2023-07-28 09:33:45 -0500

    Creacion migraciones base de datos partidos

---
+++ partidosColombia/database/migrations/2023_07_28_142421_partidos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('game_date', 50);
+            $table->string('team_1', 50);
+            $table->string('team_2', 45);
+            $table->string('team_type', 50);
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('games');
+    }
+};
