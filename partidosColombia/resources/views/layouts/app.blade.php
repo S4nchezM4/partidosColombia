df16b9e0f1f868dc299f75f3e43a64b95d76ccf8
Miguel Angel Sánchez Contreras <87663011+S4nchezM4@users.noreply.github.com>
2023-07-28 10:01:39 -0500

    incluir partidos en el menu de la persona logueada

--- partidosColombia/resources/views/layouts/app.blade.php
+++ partidosColombia/resources/views/layouts/app.blade.php
@@ -60,10 +60,12 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
+                                    <!--<li class="nav-item">-->
+                                        <a class="nav-link" href="{{ route('games.index') }}">{{ __('Partidos') }}</a>
+                                    <!--</li>-->
                                 </div>
                             </li>
                         @endguest
